d7a2487dc856017a29ea0f2282a4956be073321b
Juliano cassimiro dos santos <julianocassimiro212@gmail.com>
2022-11-29 09:51:34 -0300

    Initial commit

---
+++ Projeto (Estúdio musical)/js/js.js
@@ -0,0 +1,19 @@
+function openModal(mn)
+{
+    let modal = document.getElementById(mn);
+    
+    if (typeof modal == "undefined" || modal === null)
+        return;
+    
+        modal.style.display = 'block';
+}
+
+function closeModal(mn)
+{
+    let modal = document.getElementById(mn);
+    
+    if (typeof modal == "undefined" || modal === null)
+        return;
+        
+        modal.style.display = 'none';
+}
